3767c1743c1f5633796decc4e389db0b85e73540
The-MEDINA <147880142+The-MEDINA@users.noreply.github.com>
2026-06-16 19:39:29 -0600

    the beginning

---
+++ src/main/java/RIT/Smash/Club/Overlays/OverlaysApplication.java
@@ -0,0 +1,13 @@
+package RIT.Smash.Club.Overlays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverlaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverlaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RIT/Smash/Club/Overlays/OverlaysApplicationTests.java
@@ -0,0 +1,13 @@
+package RIT.Smash.Club.Overlays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverlaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
