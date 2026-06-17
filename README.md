7b7c0e561996f4af9fe1f7a756007b298647c1ed
The-MEDINA <147880142+The-MEDINA@users.noreply.github.com>
2026-06-16 21:24:07 -0600

    got a hello world running with a java sdk 26

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RIT-Smash-Club-Springboot-Overlay
 Throwing out the NodeCG overlay for this one
+
+you need the Java SDK version 26.
--- src/main/java/RIT/Smash/Club/Overlays/OverlaysApplication.java
+++ src/main/java/RIT/Smash/Club/Overlays/OverlaysApplication.java
@@ -2,12 +2,20 @@ package RIT.Smash.Club.Overlays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+
 public class OverlaysApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OverlaysApplication.class, args);
 	}
-
+	@GetMapping("/overlays")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 }
